a8be2aa75ddbe6ee984805ae53135b12e6c1d6cf
Nguyen duy hung <duyhungfa102@gmail.com>
2024-06-02 20:00:29 +0700

    update action

--- actions/actions.py
+++ actions/actions.py
@@ -34,14 +34,14 @@ class ActionListeningStructure(Action):
         return "action_listening_exam"
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: DomainDict) -> Coroutine[Any, Any, List[Dict[str, Any]]]:
-        # dataJson = tracker.sender_id
-        # data = json.loads(dataJson)
-        # language = data["language"]
-        # print("language : " + language)
-        # if(language == "vi"):
-        dispatcher.utter_message(text="Phần Listening của TOEIC gồm 4 phần: Part 1 là Hình ảnh, Part 2 là Hỏi & Đáp, Part 3 là Hội thoại, và Part 4 là Bài nói chuyện. Tổng cộng có 100 câu hỏi và thời gian làm bài là 45 phút.")
-        # else: 
-        #     dispatcher.utter_message(text="The Listening section of the TOEIC test consists of 4 parts: Part 1 is Photographs, Part 2 is Question-Response, Part 3 is Conversations, and Part 4 is Talks. In total, there are 100 questions, and the allotted time for completion is 45 minutes.")
+        dataJson = tracker.sender_id
+        data = json.loads(dataJson)
+        language = data["language"]
+        print("language : " + language)
+        if(language == "vi"):
+            dispatcher.utter_message(text="Phần Listening của TOEIC gồm 4 phần: Part 1 là Hình ảnh, Part 2 là Hỏi & Đáp, Part 3 là Hội thoại, và Part 4 là Bài nói chuyện. Tổng cộng có 100 câu hỏi và thời gian làm bài là 45 phút.")
+        else: 
+            dispatcher.utter_message(text="The Listening section of the TOEIC test consists of 4 parts: Part 1 is Photographs, Part 2 is Question-Response, Part 3 is Conversations, and Part 4 is Talks. In total, there are 100 questions, and the allotted time for completion is 45 minutes.")
         return []
     
 class ActionReadingStructure(Action):
@@ -50,12 +50,12 @@ class ActionReadingStructure(Action):
         return "action_ask_reading_structure"
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: DomainDict) -> Coroutine[Any, Any, List[Dict[str, Any]]]:
-        # dataJson = tracker.sender_id
-        # data = json.loads(dataJson)
-        # language = data["language"]
-        # print("language : " + language)
-        # if(language == "vi"):
-        dispatcher.utter_message(text="Phần Reading của TOEIC gồm 3 phần: Part 5 là Điền từ vào câu, Part 6 là Điền từ vào đoạn văn, và Part 7 là Đọc hiểu. Tổng cộng có 100 câu hỏi và thời gian làm bài là 75 phút.")
-        # else: 
-        #     dispatcher.utter_message(text="The Reading section of the TOEIC test comprises 3 parts: Part 5 is Incomplete Sentences, Part 6 is Text Completion, and Part 7 is Reading Comprehension. In total, there are 100 questions, and the allotted time for completion is 75 minutes.")
+        dataJson = tracker.sender_id
+        data = json.loads(dataJson)
+        language = data["language"]
+        print("language : " + language)
+        if(language == "vi"):
+            dispatcher.utter_message(text="Phần Reading của TOEIC gồm 3 phần: Part 5 là Điền từ vào câu, Part 6 là Điền từ vào đoạn văn, và Part 7 là Đọc hiểu. Tổng cộng có 100 câu hỏi và thời gian làm bài là 75 phút.")
+        else: 
+            dispatcher.utter_message(text="The Reading section of the TOEIC test comprises 3 parts: Part 5 is Incomplete Sentences, Part 6 is Text Completion, and Part 7 is Reading Comprehension. In total, there are 100 questions, and the allotted time for completion is 75 minutes.")
         return []
